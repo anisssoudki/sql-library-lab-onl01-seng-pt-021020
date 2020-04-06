4e0e8ebc51252149f58770ee5d9b9c3c8685559b
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-04-06 18:35:10 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,7 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "select books.title, books.year from books where books.series_id = series.id
+  "select title, year
+  from books
+  where series_id = 1
   order by year;"
 end
 
